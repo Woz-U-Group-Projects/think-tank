23af5e9ebed0047a93c6217dc88ab691424c8044
adehart4875 <54335481+adehart4875@users.noreply.github.com>
2020-02-18 17:54:29 -0500

    Add files via upload

---
+++ mongodb/src/main/java/com/mongodb/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.mongodb;
+
+public class CustomerRepository {
+
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
