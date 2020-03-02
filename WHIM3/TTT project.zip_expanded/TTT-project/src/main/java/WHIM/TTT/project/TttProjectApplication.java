c4584bf7af5c41c9f417c8503172a2a2ef8a12d1
Charles P JacksonBey <c.jacksonbey@yahoo.com>
2020-03-01 22:28:12 -0500

    Add files via upload

---
+++ WHIM3/TTT project.zip_expanded/TTT-project/src/main/java/WHIM/TTT/project/TttProjectApplication.java
@@ -0,0 +1,13 @@
+package WHIM.TTT.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttProjectApplication.class, args);
+	}
+
+}
---
+++ WHIM3/TTT-project/src/test/java/WHIM/TTT/project/TttProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package WHIM.TTT.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
